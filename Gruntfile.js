5f0f653d97d201c8010dd86a8b33ecbf897dfdc0
Brian Talley <b.talley@f5.com>
2015-06-19 15:54:59 -0600

    Adding Gruntfile, doxx template and jshintrc

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'lib/*.js',
+                'test/*.js'
+            ],
+            options: {
+              jshintrc: '.jshintrc'
+            }
+        },
+        tape: {
+          options: {
+            pretty: true,
+            output: 'console'
+          },
+          //files: ['test/**/*.js']
+          files: ['test/test.js']
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        },
+        doxx: {
+          all: {
+            src: 'lib',
+            target: 'docs',
+            options: {
+              title: 'LineRate Node.js REST API module'
+            }
+          }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-doxx');
+    grunt.loadNpmTasks('grunt-tape');
+
+    grunt.registerTask('test', ['jshint', 'tape']);
+    grunt.registerTask('default', ['jshint', 'tape', 'doxx']);
+
+};
